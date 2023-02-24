8393ec6777ec4a3a843e489ab4c029c4b32421f0
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-02-24 11:17:44 -0300

    email error popup

--- assets/html/js/login.js
+++ assets/html/js/login.js
@@ -1,32 +1,43 @@
 // import "./utils/petsaude";
 
 import { signup } from "./utils/userFetch.js";
+import "./utils/errorPopup.js";
 
-const password = document.getElementById("password-input");
+const emailInput = document.querySelector("#email-input");
+const passwordInput = document.getElementById("password-input");
 const icon = document.getElementById("icon");
+const loginButton = document.querySelector("#login-button");
 
 function showHide() {
-  if (password.type === "password") {
-    password.setAttribute("type", "text");
+  if (passwordInput.type === "password") {
+    passwordInput.setAttribute("type", "text");
     icon.classList.add("hide");
   } else {
-    password.setAttribute("type", "password");
+    passwordInput.setAttribute("type", "password");
     icon.classList.remove("hide");
   }
 }
 
 icon.addEventListener("click", showHide);
 
-const loginButton = document.querySelector("#login-button");
-
 loginButton.addEventListener("click", async () => {
-  const email = document.querySelector("#email-input").value;
-  const password = document.querySelector("#password-input").value;
+  const email = emailInput.value;
+  const password = passwordInput.value;
   const loginInfos = { email, password };
 
   const token = await signup(loginInfos.email, loginInfos.password);
 
   if (token) localStorage.setItem("token", token);
   else {
+    document.querySelector(".error-popup").classList.add("show");
   }
 });
+
+// emailInput.addEventListener("input", (e) => {
+//   console.log(e);
+//   document.querySelector(".error-popup").classList.remove("show");
+// });
+
+// document.querySelector(".hide-popup-btn").addEventListener("click", () => {
+//   document.querySelector(".error-popup").classList.remove("show");
+// });
