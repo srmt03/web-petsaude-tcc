dd2ab81d276c9dc5ab7d961cfdb6d6cbaf386091
Yasmin Santos <yasminnas989@gmail.com>
2023-02-24 11:00:45 -0300

    24/02/2023

--- assets/js/petsaude.js
+++ assets/js/petsaude.js
@@ -1,7 +1,16 @@
+'use strict'
+
+
+/* FUNÇÃO PARA MOSTRAR SENHA */
 const password = document.getElementById('password');
+const passwordConfirm = document.getElementById('passwordConfirm');
 const icon = document.getElementById('icon');
+const iconConfirm = document.getElementById('iconConfirm');
 
 function showHide(){
+
+    console.log(password.type)
+
     if(password.type === 'password'){
         password.setAttribute('type', 'text');
         icon.classList.add('hide')
@@ -10,5 +19,43 @@ function showHide(){
         password.setAttribute('type', 'password');
         icon.classList.remove('hide')
     }
+
 }
+
+/* Função para confirmar senha */
+
+function showHideConfirm(){
+
+    console.log(passwordConfirm.type)
+
+    if(passwordConfirm.type === 'password'){
+        passwordConfirm.setAttribute('type', 'text');
+        iconConfirm.classList.add('hide')
+    }
+    else{
+        passwordConfirm.setAttribute('type', 'password');
+        iconConfirm.classList.remove('hide')
+    }
+}
+
+/* Função para colocar completar telefone */
+
+function mascara(telefone){ 
+    if(telefone.value.length == 0)
+        telefone.value = '(' + telefone.value; 
+    if(telefone.value.length == 3)
+        telefone.value = telefone.value + ') '; 
+
+    if(telefone.value.length == 8)
+        telefone.value = telefone.value + '-'; 
+
+}
+
+
+
+
+
+
+
+
 
