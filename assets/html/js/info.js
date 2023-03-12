5d93f87adc7aac8c633ec254e87d7d879967fb27
Matheus Alves <matheus.adeoliveira11@gmail.com>
2023-03-12 02:45:52 -0300

    code formatting

---
+++ assets/html/js/info.js
@@ -0,0 +1,5 @@
+const goBackButton = document.getElementById('sign_out_button')
+goBackButton.addEventListener('click', () => {
+    const url = './profissional.html'
+    document.location.href = url
+})
